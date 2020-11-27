8176add440c542399e1d585c3da7fa8919c663ce
Andrew Durber <aidurber@gmail.com>
2020-11-27 07:16:08 +0000

    fix: avoid crash on over 100 items

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,36 @@ const getPlaylistUrl = (id: string): string =>
   `https://www.youtube.com/playlist?list=${id}`;
 
 // No way I'm typing the ytInitialData
-const extractItems = (data: object): Video[] => {
+export const extractItems = (data: object): Video[] => {
   const items = get(
     data,
     "contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents"
   );
 
-  return items.map((item: any) => {
+  return items.reduce((acc: Video[], item: any) => {
+    if (typeof item.playlistVideoRenderer === "undefined") return acc;
     const body = item.playlistVideoRenderer;
     const id = body.videoId;
-    return {
-      id,
-      name: get(body, "title.runs[0].text") || "",
-      url: `https://youtube.com/watch?v=${id}`,
-    };
-  });
+    return [
+      ...acc,
+      {
+        id,
+        name: get(body, "title.runs[0].text") || "",
+        url: `https://youtube.com/watch?v=${id}`,
+      },
+    ];
+  }, []);
+};
+
+export const createPayload = (data: object): Playlist => {
+  const maybeTitle = get(data, "metadata.playlistMetadataRenderer.title") || "";
+  let payload: Playlist | null = null;
+
+  payload = {
+    title: maybeTitle.trim(),
+    playlist: extractItems(data) || [],
+  };
+  return payload;
 };
 export default async function scrapePlaylist(
   playlistId: string
@@ -41,15 +56,9 @@ export default async function scrapePlaylist(
   if (data === null) {
     throw new Error("Unable to parse ytInitialData");
   }
-  const maybeTitle = get(data, "metadata.playlistMetadataRenderer.title") || "";
-  let payload: Playlist | null = null;
   try {
-    payload = {
-      title: maybeTitle.trim(),
-      playlist: extractItems(data) || [],
-    };
+    return createPayload(data);
   } catch (error) {
     throw new Error("Unable to parse body");
   }
-  return payload;
 }
